881f4aeb786bd7d583274f7ccb0a361aa926e14e
Wes M <34358966+wesmi@users.noreply.github.com>
2018-07-14 18:44:21 -0700

    Add beginning landing page

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Slow Web Landing</title>
+</head>
+
+<body>
+<p align="center">Main Index</p>
+
+<ul>
+	<li><a href="/weather.php">Local weather</a></li>
+</ul>
+</body>
+</html>
