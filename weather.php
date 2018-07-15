8f797768a56224978058466514cd63a9a1892a0f
Wes M <34358966+wesmi@users.noreply.github.com>
2018-07-14 19:09:04 -0700

    Opening functions of weather page

---
+++ weather.php
@@ -0,0 +1,38 @@
+<?php
+	include_once './config.php';
+	include_once './commonfunctions.php';
+
+	# Build the URL we'll use to make the weather call
+	if ($forecastApiKey != "")
+	{
+		$fullURL = "https://api.forecast.io/forecast/$forecastApiKey/$forecastLocation";
+	} else {
+		echo "Error fetching configuration data.";
+		die;
+	}
+
+	# Fetch the data
+	$response = file_get_contents($fullURL);
+
+	# Parse the HTTP headers and make sure we got a valid response
+
+	$httpResponse = parseHeaders($http_response_header);
+	if($httpResponse["response_code"] == 200)
+	{
+		$weatherData = json_decode($response, true);
+	} else {
+		echo "Error fetching weather, response code: " . $httpResponse["response_code"];
+		die;
+	}
+?>
+<html>
+<head>
+	<title>Weather</title>
+</head>
+
+<body>
+<?php
+	echo "Upcoming weather: " . $weatherData['hourly']['summary'];
+?>
+</body>
+</html>
