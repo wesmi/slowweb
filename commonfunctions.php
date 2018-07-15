10c06ef527b6b4626e62f2a3dfef19862486babc
Wes M <34358966+wesmi@users.noreply.github.com>
2018-07-14 18:55:03 -0700

    Add a common functions file

---
+++ commonfunctions.php
@@ -0,0 +1,22 @@
+<?php
+	## Common functions used among several files
+
+	# Courtesy of Mangall, http://php.net/manual/en/reserved.variables.httpresponseheader.php#117203
+	function parseHeaders( $headers )
+	{
+	    $head = array();
+	    foreach( $headers as $k=>$v )
+	    {
+	        $t = explode( ':', $v, 2 );
+	        if( isset( $t[1] ) )
+	            $head[ trim($t[0]) ] = trim( $t[1] );
+	        else
+	        {
+	            $head[] = $v;
+	            if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+	                $head['reponse_code'] = intval($out[1]);
+	        }
+	    }
+	    return $head;
+	}
+?>
