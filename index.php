627b9bd402dc2f5b6355dd998bb9e31b23945df6
Wes M <34358966+wesmi@users.noreply.github.com>
2019-05-02 16:42:32 -0700

    Modifying input method for numberic fields and removing stock module

--- index.php
+++ index.php
@@ -51,11 +51,9 @@
     <li><a href="/weather.php">Local weather</a> (<a href="/weatherlocation.php">Your local weather</a>)</li>
     <li><form method="post" action="/weather.php">Weather search: <input type="text" name="location">&nbsp;&nbsp;<input type="submit" value="Go"></form></li>
     <li><a id="baseballlink" href="/baseball.php">Baseball scores</a></li>
-    <li><form method="get" action="/baseball.php">Baseball search: <input type="text" name="m" maxlength="2" size="2">/<input type="text" name="d" maxlength="2" size="2">/<input type="text" name="y" maxlength="4" size="4">&nbsp;&nbsp;<input type="submit" value="Go"></form></li>
+    <li><form method="get" action="/baseball.php">Baseball search: <input type="tel" name="m" maxlength="2" size="2">/<input type="tel" name="d" maxlength="2" size="2">/<input type="tel" name="y" maxlength="4" size="4">&nbsp;&nbsp;<input type="submit" value="Go"></form></li>
     <li><a href="/bus.php">Bus stop information</a> (<a href="/buslocation.php">Bus stops near you</a>)</li>
-    <li><form method="post" action="/bus.php">Bus search: <input type="text" name="search">&nbsp;<input type="radio" name="type" id="Stop" value="stop" checked><label for="stop">Stop</label>&nbsp;<input type="radio" name="type" id="Route" value="route"><label for="route">Route</label>&nbsp;&nbsp;<input type="submit" value="Go"></form></li>
-    <li><a href="/stock.php">Stock report</a></li>
-    <li><form method="post" action="/stock.php">Stock search: <input type="text" name="symbol">&nbsp;&nbsp;<input type="submit" value="Go"></form></li>
+    <li><form method="post" action="/bus.php">Bus search: <input type="tel" name="search">&nbsp;<input type="radio" name="type" id="Stop" value="stop" checked><label for="stop">Stop</label>&nbsp;<input type="radio" name="type" id="Route" value="route"><label for="route">Route</label>&nbsp;&nbsp;<input type="submit" value="Go"></form></li>
 </ul>
 </body>
 </html>
