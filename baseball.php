426e81077416aa105cde7a640fe8e3991a1e7c66
Wes M <34358966+wesmi@users.noreply.github.com>
2019-05-09 10:54:36 -0700

    Add footer back to games not running

--- baseball.php
+++ baseball.php
@@ -310,7 +310,7 @@
             # If the game isn't running, put out the data with no formatting
             echo str_replace(" ", "&nbsp;", $gameStringHead) . "<br />\r\n";
             echo str_replace(" ", "&nbsp;", $gameStringTop) . "<br />\r\n";
-            echo str_replace(" ", "&nbsp;", $gameStringBottom) . "$closeDiv<br /><br />\r\n\r\n";
+            echo str_replace(" ", "&nbsp;", $gameStringBottom) . $gameStringFooter . "$closeDiv<br /><br />\r\n\r\n";
         }
 
         $closeDiv = "";
